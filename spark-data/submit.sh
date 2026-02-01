b6aaa8223eb9e9c203668c6c34dbfa75bfa7979c
cola1917 <test66@yeah.net>
2026-02-01 16:05:09 +0800

    feature 添加提交脚本

---
+++ spark-data/submit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+APP_PATH=$1
+
+if [ -z "$APP_PATH" ]; then
+  echo "Usage: ./submit.sh <path_to_py_file>"
+  exit 1
+fi
+
+spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.eventLog.enabled=true \
+  --conf spark.eventLog.dir=file:///tmp/spark-events \
+  "$APP_PATH"
